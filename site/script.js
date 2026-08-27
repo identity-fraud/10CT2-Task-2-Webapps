c8e16d2ae4ec048128b17caf6927a14141f57d02
identity-fraud <william.kim22@education.nsw.gov.au>
2026-08-27 13:30:57 +1000

    dark light mode button script (in works)

--- site/script.js
+++ site/script.js
@@ -7,3 +7,14 @@ searchForm.addEventListener('submit', (event) => {
   const query = searchBar.value.trim();
   console.log("Submitted form - Query:", query);
 });
+
+function darkLightMode() {
+  const darklight = document.getElementById("darklight").src
+  if (darklight.includes('moon')) {
+    document.getElementById("darklight").src = "assets/brightness-high-fill.svg"
+
+  }
+  else {
+    alert('dark')
+  }
+}

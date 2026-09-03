7bb99858c84d6e2dcfe3d3ec9f2fb6d1b47ed2b5
identity-fraud <william.kim22@education.nsw.gov.au>
2026-09-03 12:29:51 +1000

    broken goback img filter js

--- site/script.js
+++ site/script.js
@@ -3,15 +3,18 @@ function checkDarkLightMode() {
   const icon = document.getElementById("darklight");
   const btn = document.querySelector(".darkLightModeBtn");
   const html = document.documentElement;
+  const goback = document.querySelector(".goback");
 
   if (theme == "light") {
     icon.src = icon.dataset.srcLight;
     html.style.setProperty('color-scheme', 'light');
     btn.style.filter = "invert(0.2)";
+    goback.style.filter("invert(0)");
   } else {
     icon.src = icon.dataset.srcDark;
     html.style.setProperty('color-scheme', 'dark');
     btn.style.filter = "invert(0.8)";
+    goback.style.filter("invert(1)");
   }
 
 }
@@ -20,17 +23,20 @@ function darkLightMode() {
   const icon = document.getElementById("darklight");
   const btn = document.querySelector(".darkLightModeBtn");
   const html = document.documentElement;
+  const goback = document.querySelector(".goback");
 
   if (icon.src.includes('moon')) {
     icon.src = icon.dataset.srcLight;
     html.style.setProperty('color-scheme', 'light');
     btn.style.filter = "invert(0.2)";
     sessionStorage.setItem("theme", "light")
+    goback.style.filter("invert(0)");
   } else {
     icon.src = icon.dataset.srcDark;
     html.style.setProperty('color-scheme', 'dark');
     btn.style.filter = "invert(0.8)";
     sessionStorage.setItem("theme", "dark")
+    goback.style.filter("invert(1)");
   }
 }
 

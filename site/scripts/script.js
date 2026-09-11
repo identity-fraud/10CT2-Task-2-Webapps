0ba7a7d6ce8b484f0a2f8e307012d2fd97f9a95c
identity-fraud <william.kim22@education.nsw.gov.au>
2026-09-11 12:35:19 +1000

    change project structure

---
+++ site/scripts/script.js
@@ -0,0 +1,11 @@
+const toggleBtn = document.getElementById('theme-toggle');
+const root = document.documentElement;
+
+toggleBtn.addEventListener('click', () => {
+  const currentScheme = getComputedStyle(root).colorScheme;
+  const nextScheme = currentScheme.includes('dark') ? 'light' : 'dark';
+
+  root.style.colorScheme = nextScheme;
+  
+  localStorage.setItem('color-scheme', nextScheme);
+});
